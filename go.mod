8f1601aaea313e68be1b3462dcb4c660e417c3ce
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-07-11 11:54:25 +0100

    Publish code after initial test run
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alexellis/release-purger
 
 go 1.16
 
-require github.com/google/go-github/v36 v36.0.0
+require (
+	github.com/google/go-github/v36 v36.0.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
--- main.go
+++ main.go
@@ -2,25 +2,52 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/google/go-github/v36/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	client := github.NewClient(nil)
+	var (
+		owner     string
+		repo      string
+		tokenFile string
+		purge     bool
+	)
 
-	owner := "inlets"
-	repo := "inlets"
+	flag.StringVar(&owner, "owner", "inlets", "username or org on GitHub")
+	flag.StringVar(&repo, "repo", "inlets", "repo on GitHub")
+	flag.StringVar(&tokenFile, "token-file", "./token", "path to personal access token saved as a file")
+	flag.BoolVar(&purge, "purge", false, "purge all release artifacts")
+	flag.Parse()
+
+	t, err := os.ReadFile(tokenFile)
+	if err != nil {
+		panic(err)
+	}
+
+	token := strings.TrimSpace(string(t))
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(context.Background(), ts)
+	client := github.NewClient(tc)
+
 	fmt.Printf("Looking up %s/%s\n", owner, repo)
 
-	listOpts := &github.ListOptions{PerPage: 1, Page: 1}
+	listOpts := &github.ListOptions{PerPage: 100, Page: 1}
 
-	releases, _, err := client.Repositories.ListReleases(context.Background(), owner, repo, listOpts)
+	releases, rinfo, err := client.Repositories.ListReleases(context.Background(), owner, repo, listOpts)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%d releases\n", len(releases))
+
+	log.Printf("Next page: %s", rinfo.NextPageToken)
 
 	for _, release := range releases {
 		fmt.Printf("Releases: %s (%d)\n", release.GetName(), release.GetID())
@@ -31,8 +58,18 @@ func main() {
 				PerPage: 100,
 			})
 
-		for _, a := range assets {
-			fmt.Println(a.ID, a.GetName(), a.BrowserDownloadURL, a.GetContentType(), a.GetDownloadCount(), a.Label)
+		for _, asset := range assets {
+			fmt.Println(asset.ID, asset.GetName(),
+				asset.BrowserDownloadURL, asset.GetContentType(),
+				asset.GetDownloadCount(), asset.Label)
+
+			if purge {
+				_, err := client.Repositories.DeleteReleaseAsset(context.Background(), owner, repo, asset.GetID())
+				if err != nil {
+					panic(err)
+				}
+				fmt.Printf("%d\t%s deleted\n", asset.GetID(), asset.GetName())
+			}
 		}
 	}
 }
