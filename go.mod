f80e6254acc87955b9d33cc7418a6ff537fa23d3
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-07-27 10:12:52 +0100

    Add support for removing tags
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- go.mod
+++ go.mod
@@ -1,8 +1,20 @@
 module github.com/alexellis/release-purger
 
-go 1.16
+go 1.19
 
 require (
-	github.com/google/go-github/v36 v36.0.0
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	github.com/google/go-github/v53 v53.2.0
+	golang.org/x/oauth2 v0.10.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
-	"github.com/google/go-github/v36/github"
+	"github.com/google/go-github/v53/github"
 	"golang.org/x/oauth2"
 )
 
@@ -19,15 +20,28 @@ func main() {
 		tokenFile      string
 		purgeArtifacts bool
 		purgeReleases  bool
+		dryRun         bool
+		maxDays        int
 	)
 
-	flag.StringVar(&owner, "owner", "inlets", "username or org on GitHub")
-	flag.StringVar(&repo, "repo", "inlets-archived", "repo on GitHub")
+	flag.StringVar(&owner, "owner", "", "username or org on GitHub")
+	flag.StringVar(&repo, "repo", "", "repo on GitHub")
 	flag.StringVar(&tokenFile, "token-file", "./token", "path to personal access token saved as a file")
 	flag.BoolVar(&purgeArtifacts, "purge-artifacts", false, "purge all release artifacts")
 	flag.BoolVar(&purgeReleases, "purge-releases", false, "purge the release itself")
+	flag.BoolVar(&dryRun, "dry-run", true, "dry-run")
+	flag.IntVar(&maxDays, "max-days", 120, "maximum amount of days in age of kept releases")
 
 	flag.Parse()
+
+	if len(owner) == 0 {
+		panic("owner is required")
+	}
+	if len(repo) == 0 {
+		panic("repo is required")
+	}
+
+	maxAge := time.Duration(maxDays) * 24 * time.Hour
 
 	t, err := os.ReadFile(tokenFile)
 	if err != nil {
@@ -44,7 +58,10 @@ func main() {
 
 	listOpts := &github.ListOptions{PerPage: 100, Page: 1}
 
-	releases, rinfo, err := client.Repositories.ListReleases(context.Background(), owner, repo, listOpts)
+	releases, rinfo, err := client.Repositories.ListReleases(context.Background(),
+		owner,
+		repo,
+		listOpts)
 	if err != nil {
 		panic(err)
 	}
@@ -52,35 +69,64 @@ func main() {
 
 	log.Printf("Next page: %s", rinfo.NextPageToken)
 
+	maxDate := time.Now().Add(-maxAge)
 	for _, release := range releases {
-		fmt.Printf("Releases: %s (%d)\n", release.GetName(), release.GetID())
+		fmt.Printf(" - %s:\t%s (%d)\n", release.GetTagName(), release.GetName(), release.GetID())
 
-		assets, _, _ := client.Repositories.ListReleaseAssets(context.Background(),
-			owner, repo, release.GetID(), &github.ListOptions{
-				Page:    0,
-				PerPage: 100,
-			})
+		if purgeArtifacts {
+			assets, _, _ := client.Repositories.ListReleaseAssets(context.Background(),
+				owner, repo, release.GetID(), &github.ListOptions{
+					Page:    0,
+					PerPage: 100,
+				})
 
-		for _, asset := range assets {
-			fmt.Println(asset.ID, asset.GetName(),
-				asset.BrowserDownloadURL, asset.GetContentType(),
-				asset.GetDownloadCount(), asset.Label)
+			for _, asset := range assets {
+				fmt.Println(asset.ID, asset.GetName(),
+					asset.BrowserDownloadURL, asset.GetContentType(),
+					asset.GetDownloadCount(), asset.Label)
 
-			if purgeArtifacts {
-				_, err := client.Repositories.DeleteReleaseAsset(context.Background(), owner, repo, asset.GetID())
-				if err != nil {
-					panic(err)
+				if purgeArtifacts {
+					_, err := client.Repositories.DeleteReleaseAsset(context.Background(), owner, repo, asset.GetID())
+					if err != nil {
+						panic(err)
+					}
+
+					fmt.Printf("Artifact: %d\t%s deleted\n", asset.GetID(), asset.GetName())
 				}
-				fmt.Printf("Artifact: %d\t%s deleted\n", asset.GetID(), asset.GetName())
+
 			}
 		}
 
 		if purgeReleases {
-			_, err := client.Repositories.DeleteRelease(context.Background(), owner, repo, release.GetID())
-			if err != nil {
-				panic(err)
+
+			fmt.Printf("Max date: %v, release date: %v\n", maxDate.Round(time.Hour), release.GetCreatedAt().Time.Round(time.Hour))
+			if release.CreatedAt.Time.Before(maxDate) {
+				fmt.Printf("Deleting release: %s, name: %s, created: %s, age: %s\n",
+					release.GetTagName(), release.GetName(), release.GetCreatedAt(),
+					time.Since(release.GetCreatedAt().Time).Round(time.Hour))
+
+				fmt.Printf("Deleting: %v now\n", release.GetTagName())
+				time.Sleep(time.Millisecond * 100)
+
+				if !dryRun {
+					if _, err := client.Repositories.DeleteRelease(context.Background(), owner, repo, release.GetID()); err != nil {
+						panic(fmt.Sprintf("unable to delete tag: %s, error: %s", release.GetTagName(), err.Error()))
+
+					}
+
+					tagRef := fmt.Sprintf("refs/tags/%s", release.GetTagName())
+					if _, err := client.Git.DeleteRef(context.Background(), owner, repo, tagRef); err != nil {
+						panic(fmt.Sprintf("unable to delete tag reference: %s, error: %s", tagRef, err.Error()))
+					}
+				}
+				log.Printf("%s", release.GetTagName())
+
+				fmt.Printf("Release: %d\t%s deleted\n", release.GetID(), release.GetName())
+			} else {
+				fmt.Printf("Skipping release: %s, created: %s, age: %s\n", release.GetName(),
+					release.GetCreatedAt(),
+					time.Since(release.GetCreatedAt().Time).Round(time.Hour))
 			}
-			fmt.Printf("Release: %d\t%s deleted\n", release.GetID(), release.GetName())
 		}
 	}
 }
